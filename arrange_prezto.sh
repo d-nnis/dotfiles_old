3ce33fa04674983554440dd9fb63ee913f0ef963
Dennis <martymcfly@posteo.de>
2017-02-15 18:17:22 +0100

    gitconfig-global, re-arranged prezto (to zprezto)

--- arrange_prezto.sh
+++ arrange_prezto.sh
@@ -2,11 +2,11 @@
 set -o xtrace
 
 #git clone --recursive https://github.com/zsh-users/prezto.git $HOME/dotfiles/prezto
-#git clone --recursive https://github.com/d-nnis/prezto.git $HOME/dotfiles/prezto
+git clone --recursive https://github.com/d-nnis/prezto.git $HOME/dotfiles/zprezto
 git pull && git submodule update --init --recursive
 
 setopt EXTENDED_GLOB
-for rcfile in $HOME/dotfiles/prezto/runcoms/^README.md(.N); do
+for rcfile in $HOME/dotfiles/zprezto/runcoms/^README.md(.N); do
   if [ -f "$HOME/.${rcfile:t}" ]; then  # file exists
     mkdir -p $HOME/dotfiles_backup
     cp "$HOME/.${rcfile:t}" dotfiles_backup
