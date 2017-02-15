f4f13e42e4bd45a9cc8ba3b1aec307a1c0871635
Dennis <martymcfly@posteo.de>
2017-02-15 18:17:22 +0100

    gitconfig-global, re-arranged prezto (to zprezto)

--- arrange_dotf.sh
+++ arrange_dotf.sh
@@ -1,33 +1,48 @@
 #!/usr/bin/env bash
 set -o xtrace
 
-#setup dotfiles_backup
-if [ -f $HOME/.vimrc ]; then
-  rm $HOME/.vimrc
-fi
-ln -sv $HOME/dotfiles/vimrc $HOME/.vimrc
+files="vimrc vimperatorrc vim vimperator zprezto"
+files+=" gitconfig"
 
-if [ -d $HOME/.vim ]; then
-  rm -rf $HOME/.vim
-fi
-ln -sv $HOME/dotfiles/vim $HOME/.vim
+mkdir -p $HOME/dotfiles_backup
 
-if [ -f $HOME/.vimperatorrc ]; then
-  rm $HOME/.vimperatorrc
-fi
-ln -sv $HOME/dotfiles/vimperatorrc $HOME/.vimperatorrc
+for file in $files; do
+  echo $file
+  if [ -f $HOME/.$file ]; then
+    mv -fv $HOME/.$file $HOME/dotfiles_backup
+    #rm $HOME/.vimrc
+  fi
+  ln -sv $HOME/dotfiles/$file $HOME/.$file
+done
 
-if [ -d $HOME/.vimperator ]; then
-  rm -rf $HOME/.vimperator
-fi
-ln -sv $HOME/dotfiles/vimperator $HOME/.vimperator
 
-if [ -d $HOME/.zprezto ]; then
-  rm -rf $HOME/.zprezto
-fi
-ln -sv $HOME/dotfiles/prezto $HOME/.zprezto
-
-if [ -d $HOME/.vimperator ]; then
-  rm -rf $HOME/.vimperator
-fi
-ln -sv $HOME/dotfiles/vimperator $HOME/.vimperator
+##setup dotfiles_backup
+#if [ -f $HOME/.vimrc ]; then
+#  rm $HOME/.vimrc
+#fi
+#ln -sv $HOME/dotfiles/vimrc $HOME/.vimrc
+#
+#if [ -d $HOME/.vim ]; then
+#  rm -rf $HOME/.vim
+#fi
+#ln -sv $HOME/dotfiles/vim $HOME/.vim
+#
+#if [ -f $HOME/.vimperatorrc ]; then
+#  rm $HOME/.vimperatorrc
+#fi
+#ln -sv $HOME/dotfiles/vimperatorrc $HOME/.vimperatorrc
+#
+#if [ -d $HOME/.vimperator ]; then
+#  rm -rf $HOME/.vimperator
+#fi
+#ln -sv $HOME/dotfiles/vimperator $HOME/.vimperator
+#
+#if [ -d $HOME/.zprezto ]; then
+#  rm -rf $HOME/.zprezto
+#fi
+#ln -sv $HOME/dotfiles/prezto $HOME/.zprezto
+#
+#if [ -d $HOME/.vimperator ]; then
+#  rm -rf $HOME/.vimperator
+#fi
+#ln -sv $HOME/dotfiles/vimperator $HOME/.vimperator
