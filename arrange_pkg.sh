d7ede15a93d0839b6d20141be25077c13f8d721f
Dennis <martymcfly@posteo.de>
2017-02-21 00:10:31 +0100

    repair script

--- arrange_pkg.sh
+++ arrange_pkg.sh
@@ -1,19 +1,34 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 set -o xtrace
 
-#git clone --recursive https://github.com/zsh-users/prezto.git $HOME/dotfiles/prezto
-git clone --recursive https://github.com/d-nnis/prezto.git $HOME/dotfiles/zprezto
-git pull && git submodule update --init --recursive
+# prerequisites
+## git clone --recursive https://github.com/d-nnis/dotfiles.git
+
+sudo chsh -s /usr/bin/zsh
+
+rcfiles="zlogin zlogout zpreztorc zprofile zshenv zshrc"
 
 setopt EXTENDED_GLOB
-for rcfile in $HOME/dotfiles/zprezto/runcoms/^README.md(.N); do
-  if [ -f "$HOME/.${rcfile:t}" ]; then  # file exists
+for rcfile in $rcfiles; do
+  if [ -f "$HOME/.$rcfile" ]; then  # file exists
     mkdir -p $HOME/dotfiles_backup
-    cp "$HOME/.${rcfile:t}" dotfiles_backup
-    rm "$HOME/.${rcfile:t}"
-  elif [ -L "$HOME/.${rcfile:t}" ]; then  # file only exists as dangling symlink
-    rm "$HOME/.${rcfile:t}"
+    cp "$HOME/.$rcfile" dotfiles_backup
+    rm "$HOME/.$rcfile"
+  elif [ -L "$HOME/.$rcfile" ]; then  # file only exists as dangling symlink
+    rm "$HOME/.$rcfile"
   fi
-
-  ln -sv "$rcfile" "$HOME/.${rcfile:t}"
+#dotfiles/zprezto/runcoms/$rcfile
+  ln -sv "dotfiles/zprezto/runcoms/$rcfile" "$HOME/.$rcfile"
 done
+
+# tmux
+which tmux
+if [ "$?" -eq "1" ];then
+  sudo apt update
+  sudo apt install tmux
+fi
+
+git submodule update --init --recursive
+
+exit 0
+
