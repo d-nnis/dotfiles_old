88f8a2601f5fc06987efb0815329546b5f8e5e24
Dennis <martymcfly@posteo.de>
2017-03-29 04:21:23 +0200

    outsourced adoc to pdf toolchain

---
+++ bin/a2pdf.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+SCRIPTDIR="$( pwd )"
+
+#a2x -v -f pdf -L --asciidoc-opts="-v -b docbook -d book -f /home/dennis/owncloud_maw/Mag/kollektivistisch/lang-de.conf" --dblatex-opts="-V -T db2latex" $1
+a2x -v -f pdf -L --asciidoc-opts="-v -b docbook -d book -a lang=de" --dblatex-opts="-V -T db2latex" $1
+#a2x -v -f pdf -L --asciidoc-opts="-v -b docbook -a lang=de" --dblatex-opts="-V -T db2latex --xsl-user=fo.xsl" $1
+#a2x -v -f pdf -L --asciidoc-opts="-v -b docbook -d book" --dblatex-opts="-V -T db2latex" $1
+
