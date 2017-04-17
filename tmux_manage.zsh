b6077d6b62855166d69ec083cb62b5dbfa956f44
Dennis <martymcfly@posteo.de>
2017-04-17 03:57:03 +0200

    tmux auto-login outsourced

---
+++ tmux_manage.zsh
@@ -0,0 +1,26 @@
+#!/usr/bin/env zsh
+
+## tmux automatic switching
+
+# always start a new session
+# give information about existing sessions
+# give hint how to switch to existing session
+# destroy left session
+tmux_aware() {
+  if [[ -z $TMUX ]]; then
+    tmux
+  else  
+    sessions=$(tmux ls | awk '{print $1}' | sed 's/://')
+    num_sessions=$(echo $sessions | wc -l)
+    
+    if [[ $num_sessions > 1 ]]; then
+      echo "tmux sessions:"
+      tmux ls
+      echo "tmux at [-t#] to attach to last [/#] session"
+  fi
+fi
+#      mux new-session -d -s "$tmux_session" \; \
+#           set-option -t "$tmux_session" destroy-unattached off &> /dev/null
+#      # Attach to the 'prezto' session or to the last session used.
+#            exec tmux $_tmux_iterm_integration attach-session
+}
