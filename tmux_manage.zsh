ec6bd99833acee4f76b5af7f1e2e99eb1178d021
Dennis <martymcfly@posteo.de>
2018-05-18 20:07:00 +0200

    deal with tmux-sessions (recylce)

--- tmux_manage.zsh
+++ tmux_manage.zsh
@@ -6,13 +6,17 @@
 # give information about existing sessions
 # give hint how to switch to existing session
 # destroy left session
+
+# TODO: why and where connect to tmux!?
 tmux_aware() {
   if [[ -z $TMUX ]]; then
     tmux -2
   else  
     sessions=$(tmux ls | awk '{print $1}' | sed 's/://')
     num_sessions=$(echo $sessions | wc -l)
-    
+    head_session=$(echo $sessions | head -n1)
+
+    echo "head session: $head_session"
     if [[ $num_sessions > 1 ]]; then
       echo "tmux sessions:"
       tmux ls
